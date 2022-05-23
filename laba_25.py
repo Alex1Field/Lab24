c1b3b9a5158bce82ed0438ccf2acba6178ad4256
polev <alexey.polev@yandex.ru>
2022-05-23 18:37:49 +0300

    Alex

--- laba_25.py
+++ laba_25.py
@@ -3,7 +3,6 @@ Cтатистика канала Ted
 '''
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 
 df = pd.read_csv("TED Talks.csv")
 plt.title('Среднее кол-во просмотров за каждый год')
@@ -14,4 +13,10 @@ print(group)
 plt.plot(df['year'],df['views'])
 plt.show()
 plt.plot(df['year'],df['likes'])
+plt.show()
+df.astype({'views':'int32'}, copy=True, errors='raise')
+df.astype({'likes':'int32'}, copy=True, errors='raise')
+df['avarage']=df['likes']/df['views']*100
+print(df)
+plt.bar(df['avarage'],df['views'])
 plt.show()
